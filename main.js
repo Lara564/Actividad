3098f0885ab00ffc5508425262647e443fdf18d5
Lara564 <larideluca6@gmail.com>
2020-11-09 15:02:45 -0300

    hola

---
+++ main.js
@@ -0,0 +1,8 @@
+    ScrollReveal().reveal('.showcase');
+    ScrollReveal().reveal('.news-cards',{ delay: 300});
+    ScrollReveal().reveal('.cards-banner-one',{ delay: 300});
+    ScrollReveal().reveal('.cards-banner-two',{ delay: 300});
+    ScrollReveal().reveal('.cards-banner-three',{ delay: 300});
+    ScrollReveal().reveal('.cards-banner-four',{ delay: 300});
+    ScrollReveal().reveal('.cards-banner-five',{ delay: 300});
+  
